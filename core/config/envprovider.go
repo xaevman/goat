9bf67bb16ecf386ceadea2bf3e46d18d7cda6947
Jared Chavez <xaevman@gmail.com>
2014-01-06 14:06:24 -0800

    fixed license headers

--- core/config/envprovider.go
+++ core/config/envprovider.go
@@ -2,10 +2,11 @@
 //
 //  envprovider.go
 //
-//  Written by Jared Chavez ()
-//  Owned by Jared Chavez <xaevman@gmail.com>
+//  Copyright (c) 2013, Jared Chavez. 
+//  All rights reserved.
 //
-//  Copyright (c) 2013 Jared Chavez
+//  Use of this source code is governed by a BSD-style
+//  license that can be found in the LICENSE file.
 //
 //  -----------
 
--- core/config/iniprovider.go
+++ core/config/iniprovider.go
@@ -2,10 +2,11 @@
 //
 //  iniprovider.go
 //
-//  Written by Jared Chavez (2013-12-30)
-//  Owned by Jared Chavez <xaevman@gmail.com>
+//  Copyright (c) 2013, Jared Chavez. 
+//  All rights reserved.
 //
-//  Copyright (c) 2013 Jared Chavez
+//  Use of this source code is governed by a BSD-style
+//  license that can be found in the LICENSE file.
 //
 //  -----------
 
